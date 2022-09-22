cd13d0a985e5d4530d09512744853e7ea9c4d530
Ahmed Hesham <ahmedhesham2020s@gmail.com>
2022-09-22 12:54:28 +0200

    V2.0

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -2,6 +2,8 @@
 using DotNet5CRUDMVC.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -30,5 +32,76 @@ namespace DotNet5CRUDMVC.Controllers
         };
             return View(ViewModel);
         }
+
+        /*
+        الanti forgery بتعمل التالي
+        الاول بيكون موجود مع الفورم بتاعتك token السيرفر بعته للبراوزر
+        ولما اليوزر بيعمل post
+        ValidateAnti…
+        وظيفتها انها تتأكد ان الtoken ده موجود ومظبوط
+        وده نوع من انواع الحماية علشان تمنع ان حد يبعتلك ريكوستات من برا الابليكشن
+        */
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MovieFormViewModel model)
+        {
+            //*********************
+            if (!ModelState.IsValid)
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                return View(model);
+
+            }
+
+            var Files = Request.Form.Files;
+
+            if(!Files.Any())
+            {
+
+             model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+             ModelState.AddModelError("Poster", "please Select Movie Poster");
+             return View(model);
+
+            }
+
+            var Poster = Files.FirstOrDefault();
+            var AllowedExtension = new List<string> { ".jpg", ".png" };
+            
+            if(!AllowedExtension.Contains(Path.GetExtension(Poster.FileName).ToLower()))
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                ModelState.AddModelError("Poster", "only allowed .jpg , .png");
+                return View(model);
+            }
+
+            if(Poster.Length > 1048576)
+            {
+                model.Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync();
+                ModelState.AddModelError("Poster", "Poster can not be more than 1 MB");
+                return View(model);
+
+            }
+
+            using var DataStream = new MemoryStream();
+            await Poster.CopyToAsync(DataStream);
+
+
+            var movie = new Movie()
+            {
+                Title = model.Title,
+                GenreId = model.GenreId,
+                Year = model.Year,
+                Rate = model.Rate,
+                StoryLine = model.StoryLine,
+                Poster = DataStream.ToArray()
+            };
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+             return RedirectToAction(nameof(Index));
+        }
+
     }
 }
